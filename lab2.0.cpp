17206694f53bffcc8450a878ac4cac7c4354b281
zerusfree <115157823+zerusfree@users.noreply.github.com>
2022-10-13 16:18:40 +0300

    adding commit to master

--- lab2.0.cpp
+++ lab2.0.cpp
@@ -13,13 +13,13 @@ int main()
 	double z1; // результат першого виразу 
 	double z2; // результат другого виразу
 
-	cout << "x = "; cin >> x;
+	cout << "x = "; cin >> x;//second change 
 	z1 = 1 - 1 / 4. * sin(2 * x) * sin(2 * x) + cos(2 * x);
 	z2 = cos(x) * cos(x) + cos(x) * cos(x) * cos(x) * cos(x);
 
 	cout << endl;
 
-	cout << "z1 = " << z1 << endl;
+	cout << "z1 = " << z1 << endl;//first change 
 	cout << "z2 = " << z2 << endl;
 
 	cin.get();
