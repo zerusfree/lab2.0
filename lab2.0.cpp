e8498a30baccf154ea8901e4bb3a5c63738bcc78
zerusfree <115157823+zerusfree@users.noreply.github.com>
2022-10-13 12:32:41 +0300

    original

---
+++ lab2.0.cpp
@@ -0,0 +1,27 @@
+// Lab_2.1.cpp
+// < Петренко Назар Анатолійович >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 25
+
+#include <iostream>>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x;  // вхідний параметр
+	double z1; // результат першого виразу 
+	double z2; // результат другого виразу
+
+	cout << "x = "; cin >> x;
+	z1 = 1 - 1 / 4. * sin(2 * x) * sin(2 * x) + cos(2 * x);
+	z2 = cos(x) * cos(x) + cos(x) * cos(x) * cos(x) * cos(x);
+
+	cout << endl;
+
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
